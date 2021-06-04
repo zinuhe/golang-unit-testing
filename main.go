defd53561cd91ebf62946a5f5ce5a12f063c0636
Jimmy <zinuhe@outlook.com>
2021-06-03 22:30:19 -0400

    fixing functions

--- main.go
+++ main.go
@@ -3,35 +3,39 @@
 package main
 
 import (
-	"errors"
+	//"errors"
 	"fmt"
-	"strings"
+	//"strings"
 )
 
-func add(int number1, int number2) int {
-    
+
+func add(number1 int, number2 int) int {
+
    return number1 + number2;
 }
 
-func multiply(int number1, int number2) int {
-    
+
+func multiply(number1 int, number2 int) int {
+
    return number1 * number2;
 }
 
-func divide(int number1, int number2) float {
-    
+
+func divide(number1 int, number2 int) float32 {
+
     if number2 ==  0 {
        panic("you can not divide to 0") //or replace panic with fmt.Println();
     }
 
-   return number1 / number2;
+   return float32(number1 / number2);
 }
 
-func substract(int number1, int number2) int {
-    
+
+func substract(number1 int, number2 int) int {
+
    return number1 - number2;
 }
 
 func main() {
-  
+	fmt.Println("add 2+2=", add(2,2) )
 }
